9d964b5b28e67c3c975b46043d683f8d8ba34a11
shah 911 <mianshahzeb911@gmail.com>
2024-05-28 15:47:58 +0500

    third commit

--- src/lib/db/pinecone.ts
+++ src/lib/db/pinecone.ts
@@ -1,13 +1,7 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 
-const apiKey = process.env.PINECONE_API_KEY;
-
-if (!apiKey) {
-  throw Error("PINECONE_API_KEY is not set");
-}
-
-const pinecone = new Pinecone({
-  apiKey,
+const pc = new Pinecone({
+  apiKey: process.env.PINECONE_API_KEY!,
 });
 
-export const notesIndex = pinecone.Index("ai-chat-app");
+export const notesIndex = pc.Index("ai-chat-app");
